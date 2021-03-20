cecaa023dae50ec3f53ebe0ffda4a4c1a852a825
Tomas Utreras <tutreras100@gmail.com>
2021-03-19 20:38:27 -0400

    final version

--- client/src/views/profile/index.js
+++ client/src/views/profile/index.js
@@ -1,14 +1,13 @@
-import React, {
-  useEffect, useState
-} from 'react'
+import React, {useEffect, useState} from 'react'
 import useAlbum from '../../state/spotify/hooks/useAlbum'
 import useTrack from '../../state/spotify/hooks/useTrack'
 import Container from '../detail/containers/container'
 import Content from '../detail/components/content'
 import BackButton from '../detail/components/back'
 import Spinner from '../../components/spinner'
-
+import '../../components/profileNavLink.css'
 import { loadPlayList } from '../../api/crudApi'
+
 
 
 const Profile = (props) => {
@@ -17,20 +16,11 @@ const Profile = (props) => {
   const [usePlayList, setPlayList] = useState([]);
 
 
-  const resolvePlayList = async (prof) => {
-     const playLists =  await loadPlayList(prof);
-
-     console.log(prof);
-     setPlayList(playLists);
-  };
 
   useEffect(() => {
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-
-
   const handleMostrar = async ()=>{
     const prof = localStorage.getItem('profile') || null;
 
@@ -50,14 +40,14 @@ const Profile = (props) => {
       <Spinner show={isLoading || loading} />
       <Content>
         <BackButton link='/home'>Back</BackButton>
-        <button onClick={handleMostrar}> Show PlayList </button>
+        <button className = "profileNavLink" onClick={handleMostrar}> Show PlayList </button>
         <table>
         {
           usePlayList.map((p) => 
-          <tr key={p._id}>
-            <td><a href={p.previewUrl}>{p.track}</a></td>
-          </tr>
-            
+          <tr key={p._id} >
+            <td><a href={p.previewUrl} target="blank" style={{color: "grey", 
+            fontFamily: "georgia", fontSize: "30px", font: "bold", textDecoration: "none"}}>{p.track}</a></td>
+          </tr> 
           )
         }
         </table>
